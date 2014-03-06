de4ddf3e3d038b12a1e6e90fb558289a54398ab0
Anio-pke <anio.pke@gmail.com>
2014-03-06 02:50:58 +0100

    Continuación 1.8.x
    - Añadida la pestaña style.

---
+++ inc/languages/espanol/admin/style_module_meta.lang.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * MyBB 1.8 Spanish Language Pack
+ * Copyright 2014 MyBB Group, All Rights Reserved
+ *
+ */
+
+$l['templates_and_style'] = "Estilos y plantillas";
+
+$l['themes'] = "Estilos";
+$l['templates'] = "Plantillas";
+
+$l['can_manage_themes'] = "¿Puede configurar estilos?";
+$l['can_manage_templates'] = "¿Puede configurar plantillas?";
+
+?>
