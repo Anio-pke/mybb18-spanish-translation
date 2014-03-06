c00bdce3e1649d50f9f649911a18b52ab1ee39d0
Anio-pke <anio.pke@gmail.com>
2014-03-06 19:54:44 +0100

    Continuación 1.8.x
    - Añadida la pestaña user/Usuarios y grupos.

---
+++ inc/languages/espanol/admin/user_module_meta.lang.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * MyBB 1.8 Spanish Language Pack
+ * Copyright 2014 MyBB Group, All Rights Reserved
+ *
+ */
+
+$l['users_and_groups'] = "Usuarios y grupos";
+
+$l['users'] = "Usuarios";
+$l['groups'] = "Grupos de usuarios";
+$l['user_titles'] = "Títulos de usuario";
+$l['banning'] = "Suspensiones";
+$l['admin_permissions'] = "Permisos de admins";
+$l['mass_mail'] = "Envíos masivos";
+$l['group_promotions'] = "Promociones de grupo";
+
+$l['can_manage_users'] = "¿Puede configurar usuarios?";
+$l['can_manage_user_groups'] = "¿Puede configurar grupos de usuarios?";
+$l['can_manage_user_titles'] = "¿Puede configurar títulos de usuario?";
+$l['can_manage_user_bans'] = "¿Puede configurar suspensiones?";
+$l['can_manage_admin_permissions'] = "¿Puede configurar permisos de admins?";
+$l['can_send_mass_mail'] = "¿Puede enviar emails masivos?";
+$l['can_manage_group_promotions'] = "¿Puede configurar promociones de grupo?";
+
+?>
