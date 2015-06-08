63d363c6abcf7b77f46ef04bad81b18c5ba5ced4
Anio-pke <anio.pke@gmail.com>
2015-06-08 18:50:58 +0200

    Actualización del AdminCP MyBB 1.8.5

--- inc/languages/espanol/admin/global.lang.php
+++ inc/languages/espanol/admin/global.lang.php
@@ -355,6 +355,8 @@ El equipo de {2}";
 
 $l['comma'] = ", ";
 
+$l['search_for_a_user'] = "Buscar usuario";
+
 // If the language string for "Username" is too cramped in the ACP Login box
 // then use this to define how much larger you want the gap to be (in px)
 // $l['login_field_width'] = "0";
